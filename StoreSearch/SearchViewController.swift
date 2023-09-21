3a4a671e03c7fa3cecbc64fe304069ffb5c584f3
eriver10 <142623573+eriver10@users.noreply.github.com>
2023-09-21 17:23:23 -0400

    in Chap 32, b4 Create the UI, keyboard battery need to be recharged...

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    
+    
+    //VIEWDIDLOAD\\
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
 
 
