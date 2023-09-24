c2b64c9bc83303ab401a91ccfe366b6eba1534f9
eriver10 <142623573+eriver10@users.noreply.github.com>
2023-09-23 23:09:28 -0400

    in 36, app working with cheap fix, I don't understand...

---
+++ StoreSearch/UIImageView+DownloadImage.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView+DownloadImage.swift
+//  StoreSearch
+//
+//  Created by Grey on 9/23/23.
+//
+
+import UIKit
+
+class UIImageView_DownloadImage: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
