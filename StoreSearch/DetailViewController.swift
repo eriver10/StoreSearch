606e6cce68e03ca48a0c3f52b1fa0714d1bc7cab
eriver10 <142623573+eriver10@users.noreply.github.com>
2023-09-25 21:46:10 -0400

    Chapter 37 complete, popup added, app working

---
+++ StoreSearch/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Grey on 9/25/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    // MARK: - Actions
+    @IBAction func close() {
+      dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
