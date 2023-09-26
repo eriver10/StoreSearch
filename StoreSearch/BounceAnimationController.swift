59f3c9f6acbe9a38e7ee09c4381d2fa8df2c6262
eriver10 <142623573+eriver10@users.noreply.github.com>
2023-09-25 23:19:30 -0400

    Chap 38 complete, app working

---
+++ StoreSearch/BounceAnimationController.swift
@@ -0,0 +1,8 @@
+//
+//  BounceAnimationController.swift
+//  StoreSearch
+//
+//  Created by Grey on 9/25/23.
+//
+
+import Foundation
---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,8 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Grey on 9/25/23.
+//
+
+import Foundation
